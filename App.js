e636a2865514981f25ba9d66d75c49d7b7685db4
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-23 12:05:27 -0600

    Initial commit.

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  state = {
+    students: [
+      { name: "Michael Newman" },
+      { name: "Miek De La Rosa" },
+      { name: "Nolan Dyke" },
+      { name: "Dave Fertitta" },
+    ]
+  }
+
+  showStudents = () => this.state.students.map(student => (
+    <Text style={styles.font}> {student.name} </Text>
+  ))
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Text>Some random text.</Text>
+        <View style={styles.student}>
+          {this.showStudents()}
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +36,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  student: {
+
+  },
+  fonts: {
+    fontSize: 16,
+    color: "blue"
+  }
+})
